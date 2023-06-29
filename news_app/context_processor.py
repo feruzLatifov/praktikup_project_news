c742e3d8e155ee0732c0c4d4e259e948f5399223
Feruz Latifov <latifov0692@gmail.com>
2023-06-29 05:16:04 +0500

    news_project

---
+++ news_app/context_processor.py
@@ -0,0 +1,12 @@
+from .models import News, Category
+
+
+def latest_news(request):
+    categories = Category.objects.all()
+    latest_news = News.published.all().order_by("-publish_time")[:10]
+
+    context = {
+        "latest_news": latest_news,
+        "categories": categories
+    }
+    return context
